6e8e39122327c74e81e702e90b7aba9cbf5d1ae7
Kanishka08 <kanishkachoudhury08@gmail.com>
2024-03-26 02:25:57 +0530

    get elements by class name

---
+++ Class Name method/index.js
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+
+const fruit = document.getElementsByClassName('fruit');
+fruit[2].style.backgroundColor = 'yellow';
+
+for(let i=0;i<fruit.length;i++){
+    fruit[i].style.fontWeight = 'bold';
+=======
+
+const fruit = document.getElementsByClassName('fruit');
+fruit[2].style.backgroundColor = 'yellow';
+
+for(let i=0;i<fruit.length;i++){
+    fruit[i].style.fontWeight = 'bold';
+>>>>>>> 6c7d9268858432f454c30c4209fd34e6c449f031
+}
