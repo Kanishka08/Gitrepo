e585c34d31862d459fd59a5367127fe306e25e66
Kanishka08 <kanishkachoudhury08@gmail.com>
2024-03-26 02:24:15 +0530

    get element by id

---
+++ Understanding Dom Get Element By Id/index.js
@@ -0,0 +1,13 @@
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom =  "5px solid orange";
+
+const mainHeading= document.getElementById('main-heading');
+mainHeading.style.color = "orange";
+
+mainHeading.textContent =  "Fruit World";
+
+document.getElementById('basket-heading').style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>';
