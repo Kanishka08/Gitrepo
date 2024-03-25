17f0ee0535373bc3629a3179da2096c44fb82aa7
Kanishka08 <kanishkachoudhury08@gmail.com>
2024-03-26 03:49:59 +0530

    edit

--- How to scale/index.js
+++ How to scale/index.js
@@ -99,3 +99,24 @@ function handleFormSubmit(event) {
         parentElem.innerHTML = parentElem.innerHTML + `<li>${userDetails.username} - ${userDetails.email} - ${userDetails.phone}</li>`;
 
     }
+
+    // function handleFormSubmit(event) {
+    //   event.preventDefault();
+    
+    //   const name = event.target.username.value;
+    //   const email = event.target.email.value;
+    //   const phone = event.target.phone.value;
+    
+    //   const userDetails = {
+    //     username: name,
+    //     email: email,
+    //     phone: phone,
+    //   };
+    //   localStorage.setItem(userDetails.email, JSON.stringify(userDetails));
+    
+    //   const unorderList = document.getElementById("listItem");
+    //   const list = document.createElement("li");
+    //   list.textContent = `username: ${name}, Email: ${email}, Phone: ${phone}`;
+    
+    //   unorderList.appendChild(list);
+    // }
